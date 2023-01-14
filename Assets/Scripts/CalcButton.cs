22e0906862e39e04f1c8df0cdb4c9c36ff27c168
Anthony4pf <edeanthony14@gmail.com>
2023-01-13 22:09:06 -0800

    Add Project Files

---
+++ Assets/Scripts/CalcButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcButton : MonoBehaviour
+{
+    public Text label;
+    RectTransform _rectTransform;
+    static CalcManager _calcManager;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public RectTransform rectTransform
+    {
+        get
+        {
+            if (_rectTransform == null)
+                _rectTransform = GetComponent<RectTransform>();
+            return _rectTransform;
+        }
+    }
+    public CalcManager calcManager
+    {
+        get
+        {
+            if (_calcManager == null)
+                _calcManager = GetComponentInParent<CalcManager>();
+            return _calcManager;
+        }
+    }
+    public void OnTapped()
+    {
+        calcManager.buttonTapped(label.text[0]);
+    }
+}
